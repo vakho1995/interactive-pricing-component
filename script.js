a7e32f694bb2982a853cda8edea214046f96feb3
vakho1995 <vakhogabedava@gmail.com>
2025-06-05 16:55:51 +0400

    add JS functionality to the UI, that calculates what the user needs to pay yearly or monthly, depending on their views

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 const views = document.querySelector(".for-pageviews");
 const range = document.querySelector(".range");
-console.log(range.value);
+const price = document.querySelector(".price");
+const toggle = document.getElementById("toggle");
+let number = null;
+const viewsObj = {
+  1: "10K pageviews",
+  2: "50K pageviews",
+  3: "100K pageviews",
+  4: "500K pageviews",
+  5: "1M pageviews",
+};
+const priceObj = {
+  1: 8,
+  2: 12,
+  3: 16,
+  4: 24,
+  5: 36,
+};
+function perMonth(range) {
+  range.addEventListener("change", (e) => {
+    views.textContent = `${viewsObj[e.target.value]}`.toLocaleUpperCase();
+    price.textContent = `$ ${priceObj[e.target.value]}.00`;
+  });
+}
+
+toggle.addEventListener("change", (e) => {
+  if (toggle.checked === true) {
+    range.addEventListener("change", (e) => {
+      number = priceObj[e.target.value] * 0.75 * 12;
+      views.textContent = `${viewsObj[e.target.value]}`.toLocaleUpperCase();
+      price.textContent = `$ ${number}.00`;
+    });
+  } else {
+    perMonth(range);
+  }
+});
+perMonth(range);
